c4d697220da24e3703ec155aad98fe4a37e11ac9
Pitchounette <juliette.choudjaye@gmail.com>
2017-05-09 16:00:40 +0200

    scanner de code barre

--- FrigoMalin/app/src/main/java/com/example/ensai/frigomalin/MainActivity.java
+++ FrigoMalin/app/src/main/java/com/example/ensai/frigomalin/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ensai.frigomalin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void cliqueScan(View v){
+        startActivity(new Intent(MainActivity.this, ScannerCodeBarre.class));
+    }
+
 }
